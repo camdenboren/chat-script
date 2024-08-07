6651f2c5399c8f8d2890d0d3319b65c58d6e0240
camdenboren <boren.camden@gmail.com>
2024-08-07 11:27:20 -0500

    improve comments and formatting for prompts

--- src/chain.py
+++ src/chain.py
@@ -133,7 +133,7 @@ class MultiQueryRetriever(BaseRetriever):
         # Return unique union of retrieved documents
         return [doc for i, doc in enumerate(documents) if doc not in documents[:i]]
 
-# Define the question_answer_chain 
+# Define the contextualization prompt for summarizing chat history
 contextualize_q_system_prompt = (
     "Given a chat history and the latest user question "
     "which might reference context in the chat history, "
@@ -141,24 +141,22 @@ contextualize_q_system_prompt = (
     "without the chat history. Do NOT answer the question, "
     "just reformulate it if needed and otherwise return it as is."
 )
-contextualize_q_prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", contextualize_q_system_prompt),
-        MessagesPlaceholder("chat_history"),
-        ("human", "{input}"),
-    ]
-)
+contextualize_q_prompt = ChatPromptTemplate.from_messages([
+    ("system", contextualize_q_system_prompt),
+    MessagesPlaceholder("chat_history"),
+    ("human", "{input}"),    
+])
+
+# Define the question_answer_chain 
 system_prompt = (
     "Answer the question using the following context. If you use any information in the context, include the index (like: [1]) of the relevant Document as an in-text citation in your answer, and nothing else. Remember that each Document has two sections: page_content, and metadata- don't confuse these for indexable objects."
     "{context}"
 )
-qa_prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", system_prompt),
-        MessagesPlaceholder("chat_history"),
-        ("human", "{input}"),
-    ]
-)
+qa_prompt = ChatPromptTemplate.from_messages([
+    ("system", system_prompt),
+    MessagesPlaceholder("chat_history"),
+    ("human", "{input}"),
+])
 question_answer_chain = create_stuff_documents_chain(model, qa_prompt)
 
 def create_chain():
