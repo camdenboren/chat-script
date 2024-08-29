eec26d1714c644ff78d66aba9ae55770b1a3c3ff
camdenboren <boren.camden@gmail.com>
2024-08-29 09:16:32 -0500

    fixed build on darwin by skipping tests in fastapi build, moved overrides to overlay

--- flake.nix
+++ flake.nix
@@ -19,21 +19,35 @@
   let
     supportedSystems = [ "x86_64-linux" "aarch64-darwin" ];
     forEachSupportedSystem = function: nixpkgs.lib.genAttrs supportedSystems (system: function rec {
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = nixpkgs.legacyPackages.${system}.extend (final: prev: {
+        python311 = prev.python311.override {
+          packageOverrides = python311-final: python311-prev: {
+            fastapi = python311-prev.fastapi.overrideAttrs {
+              disabledTests = [
+                "test_fastapi_cli"
+                "test_schema_extra_examples"
+              ];
+            };
+            gradio = python311-prev.gradio.overrideAttrs (old: {
+              postInstall = (old.postInstall or "") + ''
+                cp -f ${linux-share} $out/lib/python3.11/site-packages/gradio/frpc_linux_amd64_v0.2
+                cp -f ${darwin-share} $out/lib/python3.11/site-packages/gradio/frpc_darwin_arm64_v0.2
+                chmod +x $out/lib/python3.11/site-packages/gradio/frpc_linux_amd64_v0.2
+                chmod +x $out/lib/python3.11/site-packages/gradio/frpc_darwin_arm64_v0.2
+              '';
+            });
+            langchain-chroma = prev.python311Packages.callPackage ./libs/langchain-chroma {};
+            langchain-ollama = prev.python311Packages.callPackage ./libs/langchain-ollama {};
+          };
+        };
+      });
       deps = with pkgs.python311Packages; [
-        (gradio.overrideAttrs (old: {
-          postInstall = (old.postInstall or "") + ''
-            cp -f ${linux-share} $out/lib/python3.11/site-packages/gradio/frpc_linux_amd64_v0.2
-            cp -f ${darwin-share} $out/lib/python3.11/site-packages/gradio/frpc_darwin_arm64_v0.2
-            chmod +x $out/lib/python3.11/site-packages/gradio/frpc_linux_amd64_v0.2
-            chmod +x $out/lib/python3.11/site-packages/gradio/frpc_darwin_arm64_v0.2
-          '';
-        }))
+        gradio
         langchain
         langchain-core
         langchain-community
-        (callPackage ./libs/langchain-chroma {})
-        (callPackage ./libs/langchain-ollama {})
+        langchain-chroma
+        langchain-ollama
         tiktoken
 
         # Seem unnecessary
