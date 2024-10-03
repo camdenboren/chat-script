54eb5688087f7c06135e4d388034972a22cc2553
camdenboren <9UtEfABpSSrV3g.code@mailbox.org>
2024-10-03 17:02:39 -0500

    added test_prepare_model

--- test/test_embeddings.py
+++ test/test_embeddings.py
@@ -1,8 +1,17 @@
 import unittest
+from langchain_core.embeddings.embeddings import Embeddings
 from src import embeddings, options
+
+class Document:
+    metadata = {"source": "def"}
+    page_content = "abc"
 
 class TestEmbeddings(unittest.TestCase):
     def test_opt(self):
         options.read()
         show_progress = embeddings.opt('show_progress')
         self.assertTrue(isinstance(show_progress, bool))
+
+    def test_prepare_model(self):
+        embed_model = embeddings.prepare_model()
+        self.assertTrue(isinstance(embed_model, Embeddings))
