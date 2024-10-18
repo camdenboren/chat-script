3384ab139bc6c89bf3ef02f4ec8da74466242dc1
camdenboren <9UtEfABpSSrV3g.code@mailbox.org>
2024-10-18 16:36:09 -0500

    added embeddings integ test and bumped cov to 85

---
+++ test/integration/test_embeddings_generate.py
@@ -0,0 +1,40 @@
+import os
+import tempfile
+import unittest
+from mockito import when, unstub
+from langchain_community.embeddings import FakeEmbeddings
+from langchain_community.document_loaders import DirectoryLoader, TextLoader
+from langchain_chroma import Chroma
+from src import embeddings, options
+
+class Document:
+    metadata = {"source": "def"}
+    page_content = "abc"
+
+class TestEmbeddingsGenerate(unittest.TestCase):
+    def test_generate(self):
+        def opt(option_name):
+            """Syntactic sugar for retrieving options"""
+            return options.OPTIONS['embeddings'][option_name]
+
+        mock_embed = FakeEmbeddings(size=1024)
+        options.read()
+
+        with tempfile.TemporaryDirectory() as SCRIPTS_DIR:
+            embeddings.EMBED_DIR = SCRIPTS_DIR
+            embeddings.SCRIPTS_DIR = SCRIPTS_DIR
+            
+            doc = Document()
+            docs = [doc]
+            loader = DirectoryLoader(
+                path=os.path.expanduser(SCRIPTS_DIR),
+                loader_cls=TextLoader,
+                show_progress=opt('show_progress'),
+                use_multithreading=opt('use_multithreading')
+            )
+
+            when(loader).load().thenReturn(docs)
+            when(embeddings).prepare_model().thenReturn(mock_embed)
+
+            embeddings.generate()
+            unstub()
