168d2ab64c0320b8394633d60941f644d1b76cce
camdenboren <boren.camden@gmail.com>
2024-11-11 20:13:11 -0600

    clean, fix build on darwin

--- overlay.nix
+++ overlay.nix
@@ -10,24 +10,26 @@
 }:
 
 (final: prev: {
-  python311 = prev.python311.override {
-    packageOverrides = python311-final: python311-prev: {
-      # Adding disabled test fixes build on darwin due to test_openai_schema failing with: Unclosed <MemoryObjectSendStream>
-      fastapi = python311-prev.fastapi.overrideAttrs (old: {
-        disabledTests = (old.disabledTests or [ ]) ++ [
-          "test_schema_extra_examples"
-        ];
-      });
+  python312 = prev.python312.override {
+    packageOverrides = python312-final: python312-prev: {
       # Fixes share=True by adding necessary files to $out
-      gradio = python311-prev.gradio.overrideAttrs (old: {
-        postInstall =
-          (old.postInstall or "")
-          + ''
-            cp -f ${linux-share} $out/lib/python3.11/site-packages/gradio/frpc_linux_amd64_v0.2
-            cp -f ${darwin-share} $out/lib/python3.11/site-packages/gradio/frpc_darwin_arm64_v0.2
-            chmod +x $out/lib/python3.11/site-packages/gradio/frpc_linux_amd64_v0.2
-            chmod +x $out/lib/python3.11/site-packages/gradio/frpc_darwin_arm64_v0.2
-          '';
+      gradio = python312-prev.gradio.overrideAttrs (old: {
+        disabledTests =
+          (old.disabledTests or [ ])
+          ++ [
+            "test_component_example_values"
+            "test_public_request_pass"
+            "test_private_request_fail"
+            "theme_builder_launches"
+          ];
+        #postInstall =
+          #(old.postInstall or "")
+          #+ ''
+            #cp -f ${linux-share} $out/lib/python3.11/site-packages/gradio/frpc_linux_amd64_v0.2
+            #cp -f ${darwin-share} $out/lib/python3.11/site-packages/gradio/frpc_darwin_arm64_v0.2
+            #chmod +x $out/lib/python3.11/site-packages/gradio/frpc_linux_amd64_v0.2
+            #chmod +x $out/lib/python3.11/site-packages/gradio/frpc_darwin_arm64_v0.2
+          #'';
       });
     };
   };
